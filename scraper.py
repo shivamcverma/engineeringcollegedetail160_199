14c4b4d8501fcdea0e9ade486b00b91800aa09a7
Shivam Verma <sv726544@gmail.com>
2026-01-19 16:28:45 +0530

    co

--- scraper.py
+++ scraper.py
@@ -3031,7 +3031,7 @@ def parse_faq_scholarships_section(driver, URLS):
 def scrape_mba_colleges():
     driver = create_driver()
     all_data = []
-    c_count = 160
+    c_count = 161
 
     try:
         for base_url in BASE_URL:
